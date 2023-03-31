e37382a966a4ec3dd31c7110355c0edc62a9667b
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 15:12:46 +0900

    Add components files

---
+++ components/ChatBubble.tsx
@@ -0,0 +1,7 @@
+import { ChatBubbleContainer } from './ChatBubble.style'
+
+const ChatBubble = ({ content, color }: { content: string; color: string }) => {
+    return <ChatBubbleContainer color={color}>{content}</ChatBubbleContainer>
+}
+
+export default ChatBubble
---
+++ components/GuestbookDate.style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const GuestbookDateContainer = styled.div`
+    font-size: 12px;
+    color: var(--tx-sub-dark);
+    width: 100%;
+    text-align: center;
+    margin-top: 10px;
+`
