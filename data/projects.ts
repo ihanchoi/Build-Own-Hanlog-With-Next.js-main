d839dcc9bd9cc3ab26fa66277665e8967fc57257
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 11:44:03 +0900

    Add project and resume.ts

---
+++ data/projects.ts
@@ -0,0 +1,19 @@
+import { Project } from '../lib/types'
+
+const projects: Project[] = [
+    {
+    title: 'miryang.dev',
+    description: 'My personal website written with Next.js, MDX, Emotion, Vercel',
+    url: 'https://github.com/',
+    image: '/projects/cat.gif',
+    },
+
+    {
+    title: 'XXIT 스트리트 데브 우먼 파이터',
+    description: 'XXIT에서 주최하는 스데파 세미나에서 개발자의 외주라는 주제로 발표했습니다.',
+    url: 'https://twitter.com/',
+    image: '/projects/dog.gif',
+    },
+]
+
+export default projects
