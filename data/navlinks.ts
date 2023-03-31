fa19afe7f08eba50e6a36565a6b5ed17afe7a089
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 17:33:26 +0900

    Update note link

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -2,7 +2,7 @@ const navlinks: { title: string; path: string; link: string }[] = [
   { title: 'Home', path: '/', link: '/' },
   { title: 'Blog', path: '/blog', link: '/blog' },
   { title: 'Projects', path: '/projects', link: '/projects' },
-  { title: 'Note', path: '/note', link: '/note' },
+  { title: 'Note', path: '/note', link: '/note/algorithms/index' },
   { title: 'About', path: '/about', link: '/about' },
   { title: 'Resume', path: '/resume', link: '/resume' },
   { title: 'GuestBook', path: '/guestbook', link: '/guestbook' },
