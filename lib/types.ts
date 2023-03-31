eda306711d4e19bb1e637954d44f94f327e8e896
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 11:50:36 +0900

    Add lib files

---
+++ lib/types.ts
@@ -0,0 +1,34 @@
+export type Project = {
+    title: string
+    description: string
+    url: string
+    image: string
+    }
+    
+    export type TreeNode = {
+    title: string
+    date: string
+    urlPath: string
+    children: TreeNode[]
+    }
+    
+    export type TreeRoot = TreeNode[]
+    
+    export type PathSegment = { pathName: string }
+    
+    export type tColors =
+    | '#696262'
+    | '#9088D4'
+    | '#7c889f'
+    | '#6C92BE'
+    | '#8DB596'
+    | '#c4a583'
+    | '#BB8395'
+    
+    export type tGuestbooks = {
+    [key: string]: Array<{
+        content: string
+        color: tColors
+        createdAt: string
+    }>
+    }
