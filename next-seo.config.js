c23f06988d9b344cf4eaed8ceba83809f6845283
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 15:16:18 +0900

    Add next-seo.config

---
+++ next-seo.config.js
@@ -0,0 +1,29 @@
+import metadata from './data/metadata'
+
+const SEO = {
+    titleTemplate: '%s | hanlog.dev',
+    defaultTitle: metadata.meta.title,
+    description: metadata.meta.description,
+    canonical: metadata.meta.url,
+    openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: metadata.meta.url,
+    site_name: metadata.meta.title,
+    images: [
+        {
+        url: `${metadata.meta.url}/home/main.png`,
+        width: 1280,
+        height: 720,
+        alt: `hanlog.dev`,
+        },
+    ],
+    },
+    twitter: {
+    cardType: 'summary_large_image',
+    site: metadata.meta.twitter,
+    handle: metadata.meta.twitter,
+    },
+}
+
+export default SEO
