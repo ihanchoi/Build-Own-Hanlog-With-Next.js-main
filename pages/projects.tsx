57edc4bc275f92ed5e20103c2b3de04b68ecfbe9
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 16:26:19 +0900

    Update projects pages

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,8 +9,8 @@ function Projectspage() {
     return (
     <Container>
         <NextSeo
-        title="Projects"
-        description="사이드프로젝트들을 기록합니다."
+        title="Projectssdfs"
+        description="울랄랄."
         canonical={`${metadata.meta.url}/projects`}
         openGraph={{ url: `${metadata.meta.url}/projects` }}
         />
