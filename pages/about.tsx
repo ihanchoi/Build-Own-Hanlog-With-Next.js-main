cde537d361ecaaf11058d11468e42b9cc73fafbb
ihanchoi <ihanchoi.dev@gmail.com>
2023-03-31 11:49:55 +0900

    Modify About pages

---
+++ pages/about.tsx
@@ -0,0 +1,21 @@
+import Title from '../components/Title'
+import AboutProfile from '../components/AboutProfile'
+import Container from '../components/Container'
+import metadata from '../data/metadata'
+import { NextSeo } from 'next-seo'
+
+function Aboutpage() {
+    return (
+    <Container>
+        <NextSeo
+        title="About"
+        canonical={`${metadata.meta.url}/about`}
+        openGraph={{ url: `${metadata.meta.url}/about` }}
+        />
+        <Title title="About" des="Let me introduce myself." />
+        <AboutProfile />
+    </Container>
+    )
+}
+
+export default Aboutpage
