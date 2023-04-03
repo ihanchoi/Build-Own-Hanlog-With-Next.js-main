98a0d6bc2065eb65b7792b4fffa7dc1a2f1b385d
ihanchoi <ihanchoi.dev@gmail.com>
2023-04-03 15:45:56 +0900

    update: about profile

--- components/AboutProfile.tsx
+++ components/AboutProfile.tsx
@@ -10,30 +10,32 @@ const AboutProfile = () => {
         className="round-image"
         alt="ihanchoi"
         width={300}
-        height={300}
+        height={310}
         objectFit="cover"
         />
+        
         <ProfileText>
-        새로운 경험을 좋아하는 개발자입니다.
+        <p>
+        <b>새로운 경험</b>을 좋아하는 개발자입니다.
         <br />
         <br />
-        최신 트렌드와 명료한 코드를 작성하는 것에 관심이 많고, 현재 프론트엔드에 힘을 쏟고 있습니다.
+        최신 트렌드와 명료한 코드를 작성하는 것에 관심이 많고, 현재 <b>프론트엔드</b>에 힘을 쏟고 있습니다.
         <br />
         <br />
-        세계여행을 꿈꾸고 있습니다. 
+        <b>세계여행</b>을 꿈꾸고 있습니다. 🌎🌍🌏
         <br />
-        가고 싶은 나라 🌎🌍🌏
         <br />
-        <ProfileTextSub>
-            Finland, Sweden, Italy, United Kingdom, Australia,
+        <ProfileTextSub><i>
+            → Finland, Sweden, Italy, United Kingdom, Australia,
             Austria, Switzerland, France, Japan, Germany, United States
-        </ProfileTextSub>
-        <br />
+            </i></ProfileTextSub>
+        </p>
         <br />
         <Link href="/resume" passHref>
-            <Resume>Resume</Resume>
+            <Resume>👀 This is my resume 💻</Resume>
         </Link>
         </ProfileText>
+        
     </AboutProfileContainer>
     )
 }
