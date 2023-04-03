2028703af401be18c2862b0fc2ee4ca45da46b5c
ihanchoi <ihanchoi.dev@gmail.com>
2023-04-03 12:07:21 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'layouts', 'data'],
+  },
 });
