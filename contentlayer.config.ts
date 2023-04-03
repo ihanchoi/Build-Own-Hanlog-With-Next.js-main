5b3d40554e58f12e2198ca7c652ea463f6dfb00b
ihanchoi <ihanchoi.dev@gmail.com>
2023-04-03 12:06:54 +0900

    Modify contentlayer

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -85,22 +85,21 @@ export default makeSource({
   },
 })
 
-/*
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
-export const Post = defineDocumentType(() => ({
-  name: "Post",
-  contentType: "mdx",
-  filePathPattern: `**/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-  },
-}));
+// import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
-export default makeSource({
-  contentDirPath: "posts",
-  documentTypes: [Post],
-});
-*/
+// export const Post = defineDocumentType(() => ({
+//   name: "Post",
+//   contentType: "mdx",
+//   filePathPattern: `**/*.mdx`,
+//   fields: {
+//     title: { type: "string", required: true },
+//     date: { type: "string", required: true },
+//     description: { type: "string", required: true },
+//   },
+// }));
+
+// export default makeSource({
+//   contentDirPath: "posts",
+//   documentTypes: [Post],
+// });
