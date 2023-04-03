8d1a5755fa6ccae1de69efa8e575f055977dfe7c
ihanchoi <ihanchoi.dev@gmail.com>
2023-04-03 15:47:41 +0900

    update: navlink

--- components/Nav.style.ts
+++ components/Nav.style.ts
@@ -3,55 +3,55 @@ import mq from '../styles/util/mq'
 import { css } from '@emotion/react'
 
 export const NavContainer = styled.div`
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    a {
+display: flex;
+flex-flow: row nowrap;
+align-items: center;
+a {
     text-decoration: none;
     color: var(--tx-sub-dark);
     padding-right: 20px;
     transition: 0.3s;
     font-size: 18px;
     ${mq()({
-        isplay: ['none', 'block'],
+    display: ['none', 'block'],
     })}
     &:hover {
-        color: var(--tx-accent);
-    }
-    }
-    a[data-selected='true'] {
     color: var(--tx-accent);
     }
-    svg {
+}
+a[data-selected='true'] {
+    color: var(--tx-accent);
+}
+svg {
     fill: var(--tx-sub);
     cursor: pointer;
     ${mq()({
-        display: ['block', 'none'],
+    display: ['block', 'none'],
     })}
-    }
+}
 `
 
 const dynamicVisible = ({ visible }) => {
-    if (visible) {
+if (visible) {
     return css`
-        display: flex;
+    display: flex;
     `
-    } else {
+} else {
     return css`
-        display: none;
+    display: none;
     `
-    }
+}
 }
 
 export const DropNavContainer = styled.div`
-    width: 100%;
-    height: 100vh;
-    position: absolute;
-    top: 80px;
-    left: 0;
-    flex-flow: column nowrap;
-    background: var(--bg-main);
-    a {
+width: 100%;
+height: 100vh;
+position: absolute;
+top: 80px;
+left: 0;
+flex-flow: column nowrap;
+background: var(--bg-main);
+a {
     font-size: 20px;
     text-decoration: none;
     color: var(--tx-main);
